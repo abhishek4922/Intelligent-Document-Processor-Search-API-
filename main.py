743fa14229728ac5dbee3619f24b02bf58f17f83
abhishek4922 <abhishekjain18138@gmail.com>
2025-07-29 19:52:48 +0530

    infoextractor done

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from metadata_extraction.resume_extractor import extract_resume_metadata
+from metadata_extract.resume_extractor import extract_resume_metadata
 from data_ingestion.unified_loader import load_document
 
 filepath="C:/Users/abhis/Desktop/Projects/web/proj/docextract/doc_processor/ABHISHEK_JAIN_RESUME (12).pdf"
 text = load_document(filepath)
 metadata = extract_resume_metadata(text)
-
+# doc_processor\metadata_extraction.py\resume_extractor.py
 print("Extracted Metadata:")
 for key, value in metadata.items():
     print(f"{key}: {value}")
